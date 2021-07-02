7d31d36165178d0219dbb432d22403501abe6432
TechWolf <a124302710@gmail.com>
2021-07-02 13:19:01 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,6 +5,10 @@ PORT="${PORT:-7500}"
 _update() {
 	echo "Updating Pavlov VR";
         cd ~/Steam && ./steamcmd.sh +login anonymous +force_install_dir /home/steam/pavlovserver +app_update 622970 -beta docker +exit
+        ./steamcmd.sh +login anonymous +app_update 1007 +quit
+        mkdir -p ~/.Steam/sdk64
+        cp ~/Steam/steamapps/common/Steamworks\ SDK\ Redist/linux64/steamclient.so ~/.steam/sdk64/steamclient.so
+        cp ~/Steam/steamapps/common/Steamworks\ SDK\ Redist/linux64/steamclient.so ~/pavlovserver/Pavlov/Binaries/Linux/steamclient.so
 }
 
 _run() {
