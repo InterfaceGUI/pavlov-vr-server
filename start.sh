a3bcb9352688caf024ec194019c1622c3718b41c
TechWolf <a124302710@gmail.com>
2021-07-02 13:26:50 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,6 +6,7 @@ _update() {
 	echo "Updating Pavlov VR";
         cd ~/Steam && ./steamcmd.sh +login anonymous +force_install_dir /home/steam/pavlovserver +app_update 622970 +exit
         ./steamcmd.sh +login anonymous +app_update 1007 +quit
+	echo "Updating Steam sdk64";
         mkdir -p ~/.Steam/sdk64
         cp ~/Steam/steamapps/common/Steamworks\ SDK\ Redist/linux64/steamclient.so ~/.steam/sdk64/steamclient.so
         cp ~/Steam/steamapps/common/Steamworks\ SDK\ Redist/linux64/steamclient.so ~/pavlovserver/Pavlov/Binaries/Linux/steamclient.so
